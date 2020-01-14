4eeace80af3720194f16558ec37a3d82eba1aece
Jami Kousa <jami.kousa@helsinki.fi>
2020-01-14 16:05:29 +0200

    links

--- client/components/PajaList.js
+++ client/components/PajaList.js
@@ -11,10 +11,17 @@ export default () => {
   }, [])
 
   return (
-    <div style={{ flex: 1, display: 'flex', flexDirection: 'column' }}>
+    <div style={{ flex: 1, display: 'flex', flexDirection: 'column', padding: '3em' }}>
       <WeekTable title="Current week" week="current" />
       <div style={{ paddingTop: '5em' }} />
       <WeekTable title="Next week" week="next" />
+      <div>
+        <a href="https://github.com/UniversityOfHelsinkiCS/pajat">Source code (Github)</a>
+        &nbsp; - &nbsp;
+        <a href="https://study.cs.helsinki.fi/pajat/api/calendar">API for DIY</a>
+        &nbsp; - &nbsp;
+        <a href="https://study.cs.helsinki.fi/pajat/api/ohpe/current">Example of course specific tables for iframes</a>
+      </div>
     </div>
   )
 }
