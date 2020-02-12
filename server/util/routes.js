55fbbeb35b40eb91ac6175563823c8fe46b4dffc
Henrik Nygren <nygrenh@gmail.com>
2020-02-12 14:46:12 +0200

    Implement v2 iframe

--- server/util/routes.js
+++ server/util/routes.js
@@ -1,5 +1,6 @@
 const Router = require('express')
 const calendar = require('@controllers/calendarsController')
+const calendar2 = require('@controllers/v2/calendarsController')
 
 const router = Router()
 
@@ -7,6 +8,8 @@ router.get('/', (req, res) => res.send('welcome to root'))
 
 router.get('/calendar', calendar.getAll)
 router.get('/:course/:week/', calendar.iframe)
+router.get('/v2/:course/:week/', calendar2.iframe)
+
 router.get('/:course/:week/iframe.html', calendar.iframe)
 router.get('/screen', calendar.screen)
 router.get('/luokka', (req, res) => res.type('text/plain').send('BK107'))
