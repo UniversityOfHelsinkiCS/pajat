2e000b226bdba672f041511faedc25c5fe53dc55
Jami Kousa <jami.kousa@helsinki.fi>
2020-06-02 14:11:33 +0300

    reorder routes

--- pajat/server/util/routes.js
+++ pajat/server/util/routes.js
@@ -6,14 +6,19 @@ const supervisors2 = require('@controllers/v2/supervisorsController');
 const router = Router();
 
 router.get('/', (req, res) => res.send('welcome to root'));
+router.get('/luokka', (req, res) => res.type('text/plain').send('BK107')); // Pajat always in BK107
 
-router.get('/calendar', calendar.getAll);
-router.get('/:course/:week/', calendar.iframe);
-router.get('/v2/:course/:week/', calendar2.iframe);
-
-router.get('/:course/:week/iframe.html', calendar.iframe);
-router.get('/screen', calendar.screen);
-router.get('/luokka', (req, res) => res.type('text/plain').send('BK107'));
+/**
+ * Supervisor app routes
+ */
 router.get('/supervisors/', supervisors2.getHelloWorld);
+
+/**
+ * Calendar routes
+ */
+router.get('/screen', calendar.screen);
+router.get('/:course/:week/', calendar.iframe);
+router.get('/:course/:week/iframe.html', calendar.iframe);
+router.get('/v2/:course/:week/', calendar2.iframe);
 
 module.exports = router;
