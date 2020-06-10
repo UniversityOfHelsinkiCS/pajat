48bc2e0392c2c1d12cec6d08f6529ef8fe9c4de6
Samuel Koivisto <samuel.j.koivisto@gmail.com>
2020-06-10 11:43:29 +0300

    modifying supervisors course route

--- pajat/server/controllers/v2/supervisorsController.js
+++ pajat/server/controllers/v2/supervisorsController.js
@@ -1,8 +1,9 @@
 const { ApplicationError } = require('@util/customErrors');
 const { API_KEY, SHEET_ID, fetchValues } = require('@util/common');
 
+// list of all courses
 const getCourses = async (req, res) => {
-  const url = `https://sheets.googleapis.com/v4/spreadsheets/${SHEET_ID}/values/kurssit!A1:A20?key=${API_KEY}`;
+  const url = `https://sheets.googleapis.com/v4/spreadsheets/${SHEET_ID}/values/kurssit!A1:B20?key=${API_KEY}`;
   const values = await fetchValues(url);
   if (!values) throw new ApplicationError('Try again later', 503);
   res.send(values);
