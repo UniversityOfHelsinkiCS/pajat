1ab0828a96765fb5ea3204084ed3c0b8598e4ee0
Samuel Koivisto <samuel.j.koivisto@gmail.com>
2020-06-29 12:35:17 +0300

    updating api/courses route

--- pajat/server/controllers/v2/supervisorsController.js
+++ pajat/server/controllers/v2/supervisorsController.js
@@ -1,13 +1,28 @@
 const { ApplicationError } = require('@util/customErrors');
 const { API_KEY, SHEET_ID, fetchValues } = require('@util/common');
 const Person = require('@models/Person');
+const Statistic = require('@models/Statistic');
+const Course = require('@models/Course');
+const { Op } = require('sequelize');
 
 // list of all courses
 const getCourses = async (req, res) => {
-  const url = `https://sheets.googleapis.com/v4/spreadsheets/${SHEET_ID}/values/kurssit!A1:B20?key=${API_KEY}`;
-  const values = await fetchValues(url);
-  if (!values) throw new ApplicationError('Try again later', 503);
-  res.send(values);
+  try {
+    const existingCourses = await Course.findAll();
+    if (existingCourses.length === 0) {
+      const url = `https://sheets.googleapis.com/v4/spreadsheets/${SHEET_ID}/values/kurssit!A1:B20?key=${API_KEY}`;
+      const values = await fetchValues(url);
+      const dataBlock = values.map((course, index) => ({
+        name: course[0],
+        shortName: course[1],
+      }));
+      const courseDataBlock = await Course.bulkCreate(dataBlock);
+      const result = await Course.findAll();
+      res.send(result);
+    } else res.send(existingCourses);
+  } catch (e) {
+    res.send(e);
+  }
 };
 
 const getMockPerson = async (req, res) => {
@@ -27,7 +42,9 @@ const getMockPerson = async (req, res) => {
     return res.send(createdPerson);
   }
 
-  existingPerson.updateLoginCode();
+  existingPerson.loginCode = 'UPDATED';
+
+  existingPerson.save();
 
   return res.send(existingPerson);
 };
