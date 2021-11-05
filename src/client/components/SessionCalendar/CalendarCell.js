a3559d9e3771517cb27b25c800b0e8e818a3d8aa
Kalle Ilves <kalleilv@gmail.com>
2021-11-05 08:37:17 +0200

    fix

--- src/client/components/SessionCalendar/CalendarCell.js
+++ src/client/components/SessionCalendar/CalendarCell.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { Tooltip, Box } from '@mui/material';
 
 import getInstructedCoursesAt from '../../utils/getInstructedCoursesAt';
+import instructionSessionIsAt from '../../utils/instructionSessionIsAt';
 import CourseBadge from './CourseBadge';
 import CalendarEvent from '../CalendarEvent';
 import SessionInstructorsDialog from '../SessionInstructorsDialog';
@@ -16,12 +17,16 @@ const CalendarCell = ({ date, hour, instructionSessions }) => {
   const [dialogOpen, setDialogOpen] = useState(false);
   const courses = getInstructedCoursesAt(instructionSessions, date, hour);
 
+  const sessionInstructionSessions = instructionSessions.filter((session) =>
+    instructionSessionIsAt(session, date, hour),
+  );
+
   const hasSession = courses.length > 0;
 
   return hasSession ? (
     <>
       <SessionInstructorsDialog
-        instructionSessions={instructionSessions}
+        instructionSessions={sessionInstructionSessions}
         sessionDate={date}
         startHour={hour}
         endHour={hour + 1}
@@ -30,7 +35,7 @@ const CalendarCell = ({ date, hour, instructionSessions }) => {
       />
       <CalendarEvent
         sx={{ p: 0.5 }}
-        onClick={() => setDialogOpen(false)}
+        onClick={() => setDialogOpen(true)}
         focusRipple
       >
         {courses.map((course) => (
