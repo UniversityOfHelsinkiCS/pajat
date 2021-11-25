9b568524bbae245395ba34c941f980b1173a39b2
Kalle Ilves <kalleilv@gmail.com>
2021-11-25 09:08:06 +0200

    pg date type parsing

--- src/server/utils/knex.js
+++ src/server/utils/knex.js
@@ -1,6 +1,10 @@
+const pg = require('pg');
 const createKnex = require('knex');
 
 const { KNEX_CONFIG } = require('../config');
+
+// Date type parser
+pg.types.setTypeParser(1082, (value) => value);
 
 const knex = createKnex(KNEX_CONFIG);
 
